31b3160bb1cd7d85d9d1eb705c2117409e0f2813
AlanW <AlanWisny@hotmail.com>
2018-11-28 17:49:07 +0100

    Add project files.

---
+++ Boeking/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Boeking.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
