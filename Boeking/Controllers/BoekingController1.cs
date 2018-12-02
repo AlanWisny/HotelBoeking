aa361dbd72c79246053217a38afb44dc1fd534ca
AlanW <AlanWisny@hotmail.com>
2018-12-02 20:07:18 +0100

    Initial commit

---
+++ Boeking/Controllers/BoekingController1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Boeking.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boeking.Controllers
+{
+    public class BoekingController1 : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+
+        /*
+      Maak een formulier waarmee je een nieuwe hotelkamer kunt boeken(= Create).
+      Met behulp van het PRG-pattern wordt een gebruiker doorgestuurd naar een pagina,
+          waarin een bedanktboodschap voor de boeking wordt getoond(waaronder natuurlijk de relevente gegevens voor de boeking worden getoond).
+      De boeking wordt(nog) niet opgeslagen in een database.
+      */ 
+
+        [HttpPost]
+        public IActionResult boeken(String first, String last)
+        {
+            List<HotelB> list = new List<HotelB>();
+
+            HotelB hb = new HotelB
+            {
+                naam = first,
+                achternaam = last
+            };
+            list.Add(hb);
+            return View();
+        }
+
+    }
+}
---
+++ Boeking/Models/HotelB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boeking.Models
+{
+    public class HotelB
+    {
+        public String naam { get; set; }
+        public String achternaam { get; set; }
+    }
+}
